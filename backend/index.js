c46417830ae79068bd13e6684a36ea5cfc9cef15
Jose Emmanuel B. Laurel <jose.laurel@obf.ateneo.edu>
2024-06-04 16:31:51 +0800

    EDIT: cors in index.js again2

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-  origin: ["https://joseph-lapuz.vercel.app/", "https://joseph-lapuz.onrender.com"]
+  origin: "https://joseph-lapuz.onrender.com"
 }))
 
 app.use((req, res, next) => {
