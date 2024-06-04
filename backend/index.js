33223f01f189658d7ba42ec10ea5fc689de0579c
Jose Emmanuel B. Laurel <jose.laurel@obf.ateneo.edu>
2024-06-04 15:40:00 +0800

    EDIT

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://joseph-lapuz.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PATH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   console.log(req.path, req.method)
